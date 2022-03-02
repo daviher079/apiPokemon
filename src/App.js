db82fb1582b74e360c6625ea1a70eb355944baec
David Vicente <daviher079@gmail.com>
2022-03-02 13:28:56 +0100

    Cambios

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
-import logo from './logo.svg';
+import logoPokemon from './img/logoPokemon.png'
 import './App.css';
+import Lista from './components/Lista';
+import {Fragment, useState } from 'react';
+
 
 function App() {
+  
+  const [nombres, setNombres] = useState(null);
+  const nombrePokemon = async() =>
+  {
+    let peticion ='https://pokeapi.co/api/v2/pokemon?limit=50';
+    const peticionInicial = await fetch(peticion);
+    const peticionResultados = await peticionInicial.json();
+    setNombres(peticionResultados.results);
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='miCabecera'>
+          <img src={logoPokemon} className="logo" alt="Logo"/>
+        </div>
+        
+        <div className='contenedor'>
+          {
+            nombres ? 
+            (
+              <Lista nombres={nombres} />
+
+            ): (
+              <Fragment>
+                  <input type="text" value={this.state.value} onChange={this.handleChange} />
+                <button onClick={nombrePokemon} className="btn-search">Buscar Personajes</button>
+              
+            </Fragment>
+            )
+          }
+            
+        </div>
       </header>
     </div>
   );
---
+++ src/components/Lista.js
@@ -0,0 +1,13 @@
+function Lista(props) {
+    const {nombres}=props;
+    console.log({nombres});
+    return ( 
+
+        <section className="lista">
+            <h6>Lista de los 50 primeros pokemon</h6>
+
+        </section>
+     );
+}
+
+export default Lista;
